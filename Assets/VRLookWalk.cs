aefa9b8c169dfc0f7134462e2bfef1d4e8fe3992
nwilbur95 <nickwilbur95@gmail.com>
2018-02-07 17:10:19 -0500

    Added VR LookWalk controls
    Removed FPSController to add a new camera which is controlled by looking at the ground.

---
+++ Assets/VRLookWalk.cs
@@ -0,0 +1,36 @@
+﻿//Credit for this script goes to NurFACEGAMES on YouTube
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour {
+
+	public Transform vrCamera;
+	public float toggleAngle = 30.0f;
+	public float speed = 3.0f;
+	public bool moveForward;
+
+	private CharacterController cc;
+
+	void Start () 
+	{
+		cc = GetComponent<CharacterController> ();
+	}
+
+	void Update () 
+	{
+		if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+			moveForward = true;
+
+		else
+			moveForward = false;
+
+		if (moveForward) 
+		{
+			Vector3 forward = vrCamera.TransformDirection (Vector3.forward);
+
+			cc.SimpleMove (forward * speed);
+		}
+	}
+}
